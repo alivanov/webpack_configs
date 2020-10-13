aa7ee6795ea7f4e8c7f635941f110f5d655eccca
alivanov <ivanov@intspirit.com>
2020-10-13 14:19:38 +0300

    add jquery

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import json from "./assets/json";
 import xml from "./assets/data.xml";
 import csv from "./assets/data.csv";
 import Avatar from "@/assets/avatar";
+import * as $ from "jquery";
 
 const post = new Post("Webpack post title", Avatar);
 
@@ -11,3 +12,5 @@ console.log("Post to string: " + post.toString());
 console.log("your JSON:", json);
 console.log("XML:", xml);
 console.log("CSV:", csv);
+
+$("pre").html(post.toString());
