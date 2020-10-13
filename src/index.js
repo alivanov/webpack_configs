4346ebbd44ad5d5e34a08325818868f32821b72e
alivanov <ivanov@intspirit.com>
2020-10-13 14:09:16 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Post from "@models/Post";
+import "./styles/styles.css";
+import json from "./assets/json";
+import xml from "./assets/data.xml";
+import csv from "./assets/data.csv";
+import Avatar from "@/assets/avatar";
+
+const post = new Post("Webpack post title", Avatar);
+
+console.log("Post to string: " + post.toString());
+console.log("your JSON:", json);
+console.log("XML:", xml);
+console.log("CSV:", csv);
