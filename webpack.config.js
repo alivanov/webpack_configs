ee748488f5b78eac21c5075f7b3dff607f661783
alivanov <ivanov@intspirit.com>
2020-10-13 15:07:24 +0300

    mini-css-extract plugin & cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+console.log("build mode:", process.env.NODE_ENV);
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -21,19 +26,41 @@ module.exports = {
     }
   },
   devServer: {
-    port: 4200
+    port: 4200,
+    hot: isDev
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: "./index.html"
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/favicon.ico"),
+          to: path.resolve(__dirname, "dist")
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname, "dist"),
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          "css-loader"
+        ]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
